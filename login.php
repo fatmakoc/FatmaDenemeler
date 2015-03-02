8cd9433ca475345fca78664d0525ac0d0d0c3c98
Fatma Koç <fatmakoc94@gmail.com>
2015-03-03 01:00:09 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,63 @@
+<html>
+<title>
+	Hi baby
+</title>
+<head>
+	<meta charset="utf-8">
+	<!-- bburaaya css falan gelsin -->
+  <link rel="stylesheet" href="style.css">
+</head>
+<body  background="images.jpg">
+
+  <h1 > <pre>  Fatma Denemeler </h1>
+
+<nav class="nvb" >
+      <a href="http://www.muhammedmastar.com/c-dersleri"> C++ </a>   |
+      <a href="http://www.muhammedmastar.com/csharp-dersleri"> C# </a>   |
+      <a href="http://www.muhammedmastar.com/html-dersleri"> HTML </a>   |
+      <a href="http://www.muhammedmastar.com/sql-dersleri"> SQL </a>
+
+</nav>
+
+<hr/>
+  
+  <p>
+    We have a clean page. Awesome awesome.:D<br>
+    Anyway, In there we are logining to form. <br>
+    After, this page will be redirected "VeriGoster" page.
+
+
+  </p>
+
+
+
+<form  action="veriGoster.php" method="POST">
+  <div id="LogIn">
+ <table id="KGirisi" style="width=50px; height=70px; border=2px;">
+      <tr>
+      	<td> User Name : </td>
+      	<td> <input type="text" name="ad"> </td> 
+        <pre> <pre> <pre>
+      <tr>
+      	<td> Password : </td>
+      	<td><input type="password" password="*" name="parola" > </td>
+      <tr>
+        <td>BirthYear</td>
+        <td><input type="text" name="year" > </td>
+      <tr>
+        <td> Sex : </td>
+        <td><input type="radio" name="sex" value="male" >Male </td>
+      <tr>
+        <td>  </td>
+        <td><input type="radio" name="sex" value="female" >Female </td>
+      <tr>
+        <td> <input type="submit"  onclick="alert('Completed successfully :D ')" name="buton" > </td> 
+     
+
+ </table>
+</div>
+</form>
+
+
+</body>
+</html>
