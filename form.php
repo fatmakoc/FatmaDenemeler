340a9ccf91e0033698958602fd1c656f4f886c90
Fatma Koç <fatmakoc94@gmail.com>
2015-03-03 00:59:25 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,58 @@
+<html>
+<title>
+	Hi baby
+</title>
+<head>
+	<meta charset="utf-8">
+	<!-- bburaaya css falan gelsin -->
+  <link rel="stylesheet" href="style.css">
+</head>
+<body  background="images.jpg">
+
+  <a name="sayfa_basi"> </a>
+
+  <h1 > <pre>  Fatma Denemeler </h1>
+
+<nav class="nvb" >
+      <a href="http://www.muhammedmastar.com/c-dersleri"> C++ </a>   |
+      <a href="http://www.muhammedmastar.com/csharp-dersleri"> C# </a>   |
+      <a href="http://www.muhammedmastar.com/html-dersleri"> HTML </a>   |
+      <a href="http://www.muhammedmastar.com/sql-dersleri"> SQL </a>
+
+</nav>
+
+<hr/>
+
+<nav id="nvb2">
+   |<a href="login.php"> Sign In </a>
+  </nav>
+
+<pre><pre>
+
+<p>
+    Hello, <br>
+    what's up? <br>
+    This page is test page. <br>
+    Fatmacık is studying to learn PHP . <br> 
+    Çok şaaapmayın , Fatma is burying PHP to HTML in there.<br>
+    After this time , She is a  web developer candidate.<br>
+    She doesn't know to go up or not go up her percent <br>
+    Anyway, I am filling .<br>
+    You shouldn't read baby  <br>
+    Never mind, Don't read man <br>
+    My English is very beautiiful, oh my god <br>
+    (laugh) (laugh).... :D <br>
+    In conclusion , My style is horrible, terrible and bad.I know. <br>
+    Note: Don't click links. Firstly, You must login <br>
+    
+
+</p>
+
+<pre> <pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre>
+<pre> <pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre><pre>
+
+
+<a href="#sayfa_basi"> Go to Page Up</a>
+
+</body>
+</html>
